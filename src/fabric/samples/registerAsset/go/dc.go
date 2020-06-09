28b8f1ed082c11ff0e97bc89924581d609543f2d
Figueroa, Santiago <santiagofigueroalorenzo@gmail.com>
2020-06-09 15:27:01 +0000

    Fourth CommiIX

--- src/fabric/samples/registerAsset/go/dc.go
+++ src/fabric/samples/registerAsset/go/dc.go
@@ -9,22 +9,36 @@ import (
 	"github.com/sfl0r3nz05/HFB-Testing-Environment/src/fabric/utils/go/m"
 )
 
-type SmartContract struct {
+type DeviceContract interface {
+	Init(shim.ChaincodeStubInterface) sc.Response
+	Invoke(shim.ChaincodeStubInterface) sc.Response
+	AddAsset(shim.ChaincodeStubInterface, []string) sc.Response
+	GetAsset(shim.ChaincodeStubInterface, []string) sc.Response
+	Synchro() sc.Response
+}
+
+// Define the Device Contract structure
+type ChainCode struct {
+	DeviceContract
+}
+
+func NewDeviceContract() DeviceContract {
+	return new(ChainCode)
 }
 
 /*
  * The Init method is called when the Smart Contract "fabcar" is instantiated by the blockchain network
  * Best practice is to have any Ledger initialization in separate function -- see initLedger()
  */
-func (cc *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
-	return shim.Success(nil)
+func (cc *ChainCode) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
+	return shim.Success(m.OK)
 }
 
 /*
  * The Invoke method is called as a result of an application request to run the Smart Contract "fabcar"
  * The calling application program has also specified the particular smart contract function to be called, with arguments
  */
-func (cc *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
+func (cc *ChainCode) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
 
 	// Retrieve the requested Smart Contract function and arguments
 	function, args := APIstub.GetFunctionAndParameters()
@@ -33,13 +47,15 @@ func (cc *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response
 		return cc.AddAsset(APIstub, args)
 	} else if function == "GetAsset" {
 		return cc.GetAsset(APIstub, args)
+	} else if function == "Synchro" {
+		return cc.Synchro()
 	}
 
 	return shim.Error("Invalid Smart Contract function name.")
 }
 
 //This is the main smart contract of system
-func (cc *SmartContract) AddAsset(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+func (cc *ChainCode) AddAsset(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
 	if len(args) != 2 {
 		return shim.Error("Incorrect number of arguments. Expecting 2")
 	}
@@ -54,10 +70,10 @@ func (cc *SmartContract) AddAsset(APIstub shim.ChaincodeStubInterface, args []st
 		return shim.Error(err.Error())
 	}
 
-	return shim.Success(nil)
+	return shim.Success(m.OK)
 }
 
-func (cc *SmartContract) GetAsset(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+func (cc *ChainCode) GetAsset(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
@@ -65,10 +81,14 @@ func (cc *SmartContract) GetAsset(APIstub shim.ChaincodeStubInterface, args []st
 	return shim.Success(b)
 }
 
+func (cc *ChainCode) Synchro() sc.Response {
+	return shim.Success(m.OK)
+}
+
 // The main function is only relevant in unit test mode. Only included here for completeness.
 func main() {
 	// Create a new Smart Contract
-	err := shim.Start(new(SmartContract))
+	err := shim.Start(NewDeviceContract())
 	if err != nil {
 		fmt.Printf("Error creating new Smart Contract: %s", err)
 	}
